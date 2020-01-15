d483eab73e35452c7c158803a28ec5a25feec488
Student <example@example.com>
2020-01-15 11:24:45 -0800

    add user interface for addition, subtraction, multiplication, and division.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,96 @@
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+
+// Business logic:
 
 var add = function (number1, number2) {
-    return number1 + number2;
+     return number1 + number2;
 };
 
 var subtract = function (number1, number2) {
     return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2) {
     return number1 * number2;
-}
+};
 
 var divide = function(number1, number2) {
     return number1 / number2;
-}
+};
 
-alert("add: " + add(number1, number2));
+//Everything blow this line is user interface logic:
 
-alert("subtract: " + subtract(number1, number2));
+$(document).ready(function() {
+  
+  $("form#add").submit(function() {
+    
+    var number1 = parseInt($("#first").val());
+    var number2 = parseInt($("#second").val());
+    var result = add(number1, number2);
+    $("#addOutput").text(result);
+    event.preventDefault();
+    $("#first").val("");
+    $("#second").val("");
+  });
 
-alert("multiply: " + multiply(number1, number2));
+  $("form#subtract").submit(function() {
+    var number1 = parseInt($("#subtractFirst").val());
+    var number2 = parseInt($("#subtractSecond").val());
+    var result = subtract(number1, number2);
+    $("#subtractOutput").text(result);
+    event.preventDefault();
+    $("#subtractFirst").val("");
+    $("#subtractSecond").val("");
+  });
 
-alert("divide: " + divide(number1, number2));
+  $("form#divide").submit(function() {
+    var number1 = parseInt($("#divideFirst").val());
+    var number2 = parseInt($("#divideSecond").val());
+    var result = divide(number1, number2);
+    $("#divideOutput").text(result);
+    event.preventDefault();
+    $("#divideFirst").val("");
+    $("#divideSecond").val("");
+  });
+  
+});
 
-var mass = prompt("What is your weight? (kg)");
-var height = prompt("How tall are you? (meters)")
 
-var BMI = function(mass, height) {
-  mass = parseInt(mass);
-  height = parseFloat(height);
-  heightSquared = height * height;
-  index =  mass / heightSquared
-  message = ("Your BMI is " + index);
-  return message;
-}
 
-alert(BMI(mass, height));
 
-var gal = prompt("Enter gal");
-var convert = function (x) {
-liter = x * 3.785;
-return liter;
-}
 
-alert(convert(gal));
 
-var cel = prompt("Enter celsius");
-var celToFahr = function (cel) {
-return cel * 1.8 + 32;
-}
+//Conversion functions
 
-alert(celToFahr(cel));
+// var BMI = function(mass, height) {
+//   mass = parseInt(mass);
+//   height = parseFloat(height);
+//   heightSquared = height * height;
+//   index =  mass / heightSquared
+//   message = ("Your BMI is " + index);
+//   return message;
+// }
 
-var fahr = prompt("Enter fahrenheit");
-var fahrToCel = function(fahr) {
-	cel = (parseInt(fahr) - 32) / 1.8;
-	return cel;
-}
+// alert(BMI(mass, height));
 
-alert(fahrToCel(fahr));
+// var gal = prompt("Enter gal");
+// var convert = function (x) {
+// liter = x * 3.785;
+// return liter;
+// }
+
+// alert(convert(gal));
+
+// var cel = prompt("Enter celsius");
+// var celToFahr = function (cel) {
+// return cel * 1.8 + 32;
+// }
+
+// alert(celToFahr(cel));
+
+// var fahr = prompt("Enter fahrenheit");
+// var fahrToCel = function(fahr) {
+// 	cel = (parseInt(fahr) - 32) / 1.8;
+// 	return cel;
+// }
+
+// alert(fahrToCel(fahr));
